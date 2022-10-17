2a8888d81345ec35eff491657b452706a81bbee9
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-10-17 13:03:15 -0500

    feat: Determine the percentage of compliance with the SLAs

--- src/SLABreach.php
+++ src/SLABreach.php
@@ -12,14 +12,32 @@ class SLABreach
 
     public bool $breached = false;
 
+    public float|int $fulfillment = 0;
+
     public function __construct(string $name, string $string_duration)
     {
         $this->name = $name;
         $this->breached_after = CarbonInterval::fromString($string_duration);
     }
 
-    public function check(CarbonInterval $current_interval)
+    public function check(CarbonInterval $current_interval): void
     {
         $this->breached = $current_interval->cascade()->totalSeconds > $this->breached_after->cascade()->totalSeconds;
+        $this->setProgress($current_interval);
+    }
+
+    public function setProgress(CarbonInterval $current_interval): void
+    {
+        $start = $current_interval->cascade()->totalSeconds;
+        $end = $this->breached_after->cascade()->totalSeconds;
+
+        if ($start > $end)
+        {
+            $this->fulfillment = 0;
+        }
+        else
+        {
+            $this->fulfillment = 100 - (($start * 100) / $end);
+        }
     }
 }
